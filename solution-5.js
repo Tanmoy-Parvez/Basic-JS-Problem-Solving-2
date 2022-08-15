cb5d3f0c0449f750e285d95d57b3e4175db6a744
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-08-16 00:15:00 +0600

    problem-5 solved

---
+++ solution-5.js
@@ -0,0 +1,11 @@
+/* 
+5. Write a function that takes a number and returns its square.
+*/
+
+function make_square(number) {
+    const square = number * number;
+    return square;
+}
+
+const result = make_square(5);
+console.log(result);
